1fb7d4e50e504afcc520f50267eb297c1788eca5
Gary Williams <gary@lbm.co>
2021-04-26 16:17:12 -0400

    increased swipe tolerance

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import Vue2TouchEvents from "vue2-touch-events";
 
-Vue.use(Vue2TouchEvents);
+Vue.use(Vue2TouchEvents, {
+  swipeTolerance: 80
+});
 
 new Vue({
   render: h => h(App)
